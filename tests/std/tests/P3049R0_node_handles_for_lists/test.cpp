812a03e31827003ebb2abdecf9d31276cfa40742
MFHava <mfh@live.at>
2024-02-19 12:07:30 +0100

    minor changes

--- tests/std/tests/P3049R0_node_handles_for_lists/test.cpp
+++ tests/std/tests/P3049R0_node_handles_for_lists/test.cpp
@@ -1,86 +1,87 @@
+#include <list>
 #include <cassert>
 #include <cstddef>
-#include <cstdio>
-#include <initializer_list>
 #include <iterator>
-#include <memory>
-#include <type_traits>
-#include <utility>
-#include <list>
 #include <forward_list>
+#include <initializer_list>
 
-bool allocation_allowed{true};
-std::size_t allocation_count{0};
-std::size_t construct_count{0};
+bool allocations_allowed{true};
+std::size_t allocations{0}, constructions{0};
 
-template <class T>
-struct tracked_allocator {
+template<typename T>
+struct allocator final {
     using value_type = T;
 
-    constexpr tracked_allocator() noexcept = default;
-    template <class U>
-    constexpr tracked_allocator(tracked_allocator<U> const&) noexcept {}
+    constexpr
+    allocator() noexcept =default;
+    template<typename U>
+    constexpr
+    allocator(const allocator<U> &) noexcept {}
 
-    T* allocate(size_t n) {
-        assert(allocation_allowed);
+
+    auto allocate(std::size_t n) -> T * {
+        assert(allocations_allowed);
         T* ptr = std::allocator<T>{}.allocate(n);
-        ++allocation_count;
+        ++allocations;
         return ptr;
     }
 
-    void deallocate(T* ptr, size_t n) noexcept {
-        assert(allocation_allowed);
+    void deallocate(T * ptr, std::size_t n) noexcept {
+        assert(allocations_allowed);
+        assert(allocations);
         std::allocator<T>{}.deallocate(ptr, n);
-        --allocation_count;
+        --allocations;
     }
 
-    template <class U, class... Args>
-    void construct(U* ptr, Args&&... args) {
+
+    template<typename U, typename... Args>
+    void construct(U * ptr, Args &&... args) {
         std::allocator<T> alloc;
         std::allocator_traits<std::allocator<T>>::construct(alloc, ptr, std::forward<Args>(args)...);
-        ++construct_count;
+        ++constructions;
     }
 
-    template <class U>
-    void destroy(U* ptr) {
+    template<typename U>
+    void destroy(U * ptr) {
+        assert(constructions);
         std::allocator<T> alloc;
         std::allocator_traits<std::allocator<T>>::destroy(alloc, ptr);
-        --construct_count;
+        --constructions;
     }
 
-    template <class U>
-    bool operator==(tracked_allocator<U> const&) const noexcept {
-        return true;
-    }
-    template <class U>
-    bool operator!=(tracked_allocator<U> const&) const noexcept {
-        return false;
-    }
+    template<typename U>
+    friend
+    auto operator==(const allocator &, const allocator<U> &) noexcept -> bool { return true; }
 };
 
 template<typename Func>
 void no_allocation_scope(Func func) {
-    allocation_allowed = false;
+    allocations_allowed = false;
     func();
-    allocation_allowed = true;
+    allocations_allowed = true;
 }
 
-bool test_equal(const std::list<int, tracked_allocator<int>> & l, std::initializer_list<int> ilist) {
+bool test_equal(const std::list<int, allocator<int>> & l, std::initializer_list<int> ilist) {
     //check bidirectional to ensure all internal pointers are correctly set up
     return std::equal(l.begin(), l.end(), ilist.begin(), ilist.end())
         && std::equal(l.rbegin(), l.rend(), std::make_reverse_iterator(ilist.end()), std::make_reverse_iterator(ilist.begin()));
 }
 
-bool test_equal(const std::forward_list<int, tracked_allocator<int>> & l, std::initializer_list<int> ilist) {
+bool test_equal(const std::forward_list<int, allocator<int>> & l, std::initializer_list<int> ilist) {
     //check bidirectional to ensure all internal pointers are correctly set up
     return std::equal(l.begin(), l.end(), ilist.begin(), ilist.end());
 }
 
+template<typename Allocator>
+auto size(const std::forward_list<int, Allocator> & l) -> std::size_t {
+    return static_cast<std::size_t>(std::distance(l.begin(), l.end()));
+}
+
 void test_list() {
     {
-        std::list<int, tracked_allocator<int>> l{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        std::list<int, allocator<int>> l{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assert(l.size() == 10);
-        assert(allocation_count >= l.size());
+        assert(allocations >= l.size());
 
         no_allocation_scope([&] {
             auto nh_front{l.extract(l.begin())};
@@ -115,7 +116,7 @@ void test_list() {
             assert(test_equal(l, {9, 1, 2, 3, 5, 4, 6, 7, 8, 0}));
         });
 
-        std::list<int, tracked_allocator<int>> empty;
+        std::list<int, allocator<int>> empty;
         no_allocation_scope([&] {
             auto nh{l.extract(l.begin())};
             assert(nh.value() == 9);
@@ -127,20 +128,15 @@ void test_list() {
         assert(!nh.empty());
     }
 
-    assert(allocation_count == 0);
-    assert(construct_count == 0);
-}
-
-template<typename Allocator>
-auto size(const std::forward_list<int, Allocator> & l) -> std::size_t {
-    return static_cast<std::size_t>(std::distance(l.begin(), l.end()));
+    assert(allocations == 0);
+    assert(constructions == 0);
 }
 
 void test_forward_list() {
     {
-        std::forward_list<int, tracked_allocator<int>> l{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        std::forward_list<int, allocator<int>> l{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assert(size(l) == 10);
-        assert(allocation_count >= size(l));
+        assert(allocations >= size(l));
 
         no_allocation_scope([&] {
             auto nh_front{l.extract_after(l.before_begin())};
@@ -175,7 +171,7 @@ void test_forward_list() {
             assert(test_equal(l, {9, 1, 2, 3, 5, 4, 6, 7, 8, 0}));
         });
 
-        std::forward_list<int, tracked_allocator<int>> empty;
+        std::forward_list<int, allocator<int>> empty;
         no_allocation_scope([&] {
             auto nh{l.extract_after(l.before_begin())};
             assert(nh.value() == 9);
@@ -187,8 +183,8 @@ void test_forward_list() {
         assert(!nh.empty());
     }
 
-    assert(allocation_count == 0);
-    assert(construct_count == 0);
+    assert(allocations == 0);
+    assert(constructions == 0);
 }
 
 int main() {
